ea35f31bdfc2a9c38bcb4300e77b4af1a76c4da7
heyi-wang <heyi.wang@acdine.com>
2016-01-12 17:08:30 +0800

    OT-411: Place the input cursor at the end of the input field of a input dialog. r=jianmin

--- js/views/setup_calendar.js
+++ js/views/setup_calendar.js
@@ -216,6 +216,8 @@ SetupCalendar.prototype = {
 
   _openDialog: function(option) {
     this.dialogController.once('opened', () => {
+      var diaInput = document.activeElement;
+      diaInput.setSelectionRange(diaInput.value.length, diaInput.value.length);
     });
     this.dialogController.once('closed', () => {
       this.rootElement.focus();
