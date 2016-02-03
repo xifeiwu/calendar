9b7726c19ec962a9758fb3f2c08e391c530de9f1
heyi-wang <heyi.wang@acdine.com>
2016-02-03 10:13:51 +0800

    CORE-4166: Notifications are replaced by dialogs while calendar is active. r=jianmin

--- js/controllers/dialog.js
+++ js/controllers/dialog.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
 'use strict';
 
 var Responder = require('responder');
+var nextTick = require('next_tick');
 require('shared/h5-dialog/dist/amd/script');
 
 var DEBUG = false;
@@ -10,12 +11,20 @@ var DEBUG = false;
 function DialogController(app) {
   this.app = app;
   this.dialog = document.getElementById('calendar-dialog');
+  this.notiDialog = document.getElementById('notification-dialog');
 
   this.dialog.on('h5dialog:opened', function() {
     if (DEBUG) {
       console.log('DialogController opened');
     }
     this.emit('opened');
+  }.bind(this));
+
+  this.notiDialog.on('h5dialog:opened', function() {
+    if (DEBUG) {
+      console.log('DialogController opened');
+    }
+    this.emit('notiOpened');
   }.bind(this));
 
   this.dialog.on('h5dialog:closed', function() {
@@ -26,21 +35,23 @@ function DialogController(app) {
     this.emit('closed');
   }.bind(this));
 
-  this.dialog.addEventListener('blur', function(evt) {
+  this.notiDialog.on('h5dialog:closed', function() {
     if (DEBUG) {
-      console.log('DialogController blur');
+      console.log('DialogController closed');
     }
-    if (this.dialog.classList.contains('opened')) {
-      this.dialog.close();
-    }
-    this.emit('blur');
+    this.emit('notiClosed');
   }.bind(this));
 
   this.dialog.dialogTextInput.addEventListener('blur', function(evt) {
     if (DEBUG) {
       console.log('DialogController blur');
     }
-    this.emit('input-blur');
+    nextTick(() => {
+      if (this.notiDialog.classList.contains('closed')) {
+        console.log('emitting input-blur');
+        this.emit('input-blur');
+      }
+    });
   }.bind(this));
 
   Responder.call(this);
@@ -79,6 +90,35 @@ DialogController.prototype = {
     }
   },
 
+  notiShow: function(option) {
+    if (DEBUG) {
+      console.log(JSON.stringify(option));
+    }
+
+    if (!option || !option.dialogType ||
+      !(option.softKeysHandler || option.inputSoftKeysHandler)) {
+      console.warn('DialogController warnning: empty option!');
+    }
+
+    if (option.softKeysHandler) {
+      softkeyHandler.register(this.notiDialog, option.softKeysHandler);
+    }
+    if (option.inputSoftKeysHandler) {
+      softkeyHandler.register(this.notiDialog.dialogTextInput,
+        option.inputSoftKeysHandler);
+    }
+
+    this.notiDialog.open({
+      header: option.header || '',
+      dialogType: option.dialogType || 'prompt',
+      message: option.message || '',
+      initialValue: option.initialValue || ''
+    });
+    if (DEBUG) {
+      console.log('DialogController call open');
+    }
+  },
+
   setInputValue: function(text) {
     this.dialog.dialogTextInput.value = text;
   },
@@ -100,6 +140,13 @@ DialogController.prototype = {
       console.log('DialogController call close');
     }
     this.dialog.close();
+  },
+
+  notiClose: function(option) {
+    if (DEBUG) {
+      console.log('DialogController call close');
+    }
+    this.notiDialog.close();
   }
 };
 
