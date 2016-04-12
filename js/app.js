2d048ad3401e991c35cc640fb0042eb2a528e098
heyi-wang <heyi.wang@acdine.com>
2016-04-12 16:37:36 +0800

    CORE-2378: See if currentPath starts with event/list rather than equals to it. r=manson

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@ module.exports = {
       // If users are in event-list page while changing the format, whole page
       // is required to be reloaded since events' list are drew at the beginning
       // of loading this page and changing the time format may reset the width.
-      if (router.currentPath === '/event/list/') {
+      if (router.currentPath.startsWith('/event/list/')) {
         router.go(router.currentPath);
       }
     });
